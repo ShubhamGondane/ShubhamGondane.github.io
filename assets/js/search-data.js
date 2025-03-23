94350fc9350785cef01a353d81cd3d1cc4028a00
ShubhamGondane <shubhambg1995@gmail.com>
2025-03-23 01:19:04 +0000

    Deploying to gh-pages from @ ShubhamGondane/ShubhamGondane.github.io@9488a1eeb5412ac533f5291b9e058524a19f701c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of half-baked side projects and fun stuff.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
