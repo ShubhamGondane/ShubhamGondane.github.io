dacab00fca5f35a869d7d306f808ff13eb78a22d
ShubhamGondane <ShubhamGondane@users.noreply.github.com>
2025-06-20 03:14:58 +0000

    Deploying to gh-pages from @ ShubhamGondane/ShubhamGondane.github.io@dd2021eb115891f752214b1b48d5327de715e289 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-netflix-data-engineering-open-forum-2025",
+        },{id: "post-automating-data-pipeline-recovery",
+      
+        title: 'Automating Data Pipeline Recovery <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://blog.det.life/automating-data-pipeline-recovery-366eeaf60154?source=rss-b2864b4119c4------2", "_blank");
+        
+      },
+    },{id: "post-netflix-data-engineering-open-forum-2025",
       
         title: "Netflix Data Engineering Open Forum 2025",
       
